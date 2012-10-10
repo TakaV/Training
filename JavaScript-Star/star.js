a8ab7cb38a3f287e889e9f5e3b7f940af858843c
taka <taka.k.kingfox@gmail.com>
2012-10-10 21:03:28 +0900

    課題2修正

--- JavaScript-Star/star.js
+++ JavaScript-Star/star.js
@@ -9,12 +9,21 @@ window.requestAnimationFrame = (function(){
         };
 })();
 
+var star1, star2, star3, star4, star5;
+$(function() {
+    star1 = $("#star1");
+    star2 = $("#star2");
+    star3 = $("#star3");
+    star4 = $("#star4");
+    star5 = $("#star5");
+});
+
 var mouseStalker = function (event) {
-    var starIds = [ '#star1', '#star2', '#star3', '#star4', '#star5' ];
+    var starIds = [ star1, star2, star3, star4, star5 ];
 
     var addPoint = 0;
     for (var i = 0; i < starIds.length; ++i) {
-        $(starIds[i]).css("left", event.pageX + addPoint).css("top", event.pageY + addPoint);
+        starIds[i].css("left", event.pageX + addPoint).css("top", event.pageY + addPoint);
         addPoint += 15;
     }
 }
